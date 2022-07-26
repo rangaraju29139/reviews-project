fe0227d99fa186a502d84b02fd2f5d2ff1552624
rangaraju29139 <rangaraju29139@gmail.com>
2022-07-26 19:24:42 +0530

    added functionality for buttons and fetching data from the reviews array

--- app.js
+++ app.js
@@ -4,36 +4,74 @@ const reviews = [
     id: 1,
     name: "susan smith",
     job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: "anna johnson",
     job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
     name: "peter jones",
     job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
     name: "bill anderson",
     job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// select elements
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+const prev = document.querySelector(".prev-btn");
+const next = document.querySelector(".next-btn");
+const surprise = document.querySelector(".random-btn");
+
+let currentItem = 1;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const item = reviews[currentItem];
+  showPerson(item);
+});
+function showPerson(person) {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+prev.addEventListener("click", function () {
+  currentItem--;
+  let len = reviews.length;
+  currentItem = (currentItem + len) % len;
+  item = reviews[currentItem];
+  showPerson(item);
+});
+next.addEventListener("click", () => {
+  currentItem++;
+  let len = reviews.length;
+  currentItem = (currentItem + len) % len;
+  item = reviews[currentItem];
+  showPerson(item);
+});
+
+surprise.addEventListener("click", () => {
+  currentItem = getRandomNumber();
+  item = reviews[currentItem];
+  showPerson(item);
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * reviews.length);
+}
